349e07a54fd3b665193784e666d4f2d7d81d5391
Romulo Santos <romulosnik@hotmail.com>
2024-06-06 14:47:57 -0300

    Criando Autenticação e Autorização usando JWT

---
+++ Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    //Token Json Web Token
+    public static string JwtKey = "KEmfqfZSfmvPRYarZYKIKDqontUVLvuK";
+
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso-ApiKEmfqfZSfmvPRYar";
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x => { 
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 //Adicionar Controlers
 builder
     .Services
@@ -12,8 +33,15 @@ builder
     
 //Injetar contexto
 builder.Services.AddDbContext<BlogDataContext>();
+//Injetar TokenService
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
+//Ativar Authenticação e Autorização 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
